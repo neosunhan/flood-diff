cd1d5ec113de76e4385ef2561b1bba759978faa5
neosunhan <neosunhan@gmail.com>
2025-04-05 21:46:54 +0800

    Add abstract and overview to README

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # Physics Informed Generative AI for High Resolution Flood Mapping
-Pytorch implementation and pretrained models
+## Abstract
+Traditional hydrodynamic models generate high-resolution flood maps through fine-grid discretization; however, these models are computationally intensive and impractical for real-time applications. In this report, we propose a novel approach that leverages diffusion models to perform super-resolution on coarse-grid flood maps, with the objective of achieving the accuracy of fine grid flood maps while significantly reducing inference time. Our experimental results demonstrate that diffusion models can substantially decrease the computational time required to produce high-fidelity flood maps without compromising on accuracy. Moreover, diffusion models exhibit superior generalizability across different catchments, with transfer learning further accelerating adaptation to new geographic regions. Finally, by incorporating physics-informed inputs into the model, our approach addresses the common limitation of black-box behavior in machine learning, thereby enhancing interpretability.
+
+![A hydrodynamic model is used to generate a coarse-grid flood map, which is then passed into a diffusion model together with a DEM to generate a super-resolution flood map](./images/proposed_approach.png "High Level Overview")
+<p style="text-align: center;">Figure 1: High level overview of the process</p>
 
 ## Pretrained models
 This section contains the latent diffusion model checkpoints. `_gen.pth` files contain the model parameters and are needed for inference, while `_opt.pth` files contain the saved optimizer and are needed for further transfer learning. The pretrained autoencoder checkpoint can be found [here](https://drive.google.com/drive/folders/1OurEy9H589z9jXHj-cXtZ1pfyUcCQlIJ?usp=sharing).
 
-| Training Catchment | Download |
-| :----------------: | :------: |
-| Wollombi           | [checkpoint](https://drive.google.com/drive/folders/1lu39Zfs-wA01czTXvnoOOI5LSeClRZk2?usp=sharing) |
-| Burnett            | [checkpoint](https://drive.google.com/drive/folders/1mp4n1uPkRVhe27OpUUnkoE726Wx2agYL?usp=sharing) |
-| Chowilla           | [checkpoint](https://drive.google.com/drive/folders/1m5hnVxXkf6R8kkpJbeKljij_TqobRqSf?usp=sharing) |
+| Training Catchment | CG-FG MSE | SR-FG-MSE | % change | Download |
+| :----------------: | :-------: | :-------: | :------: | :------: |
+| Wollombi | 344.2 | 33.7 | -90.19 | [checkpoint](https://drive.google.com/drive/folders/1lu39Zfs-wA01czTXvnoOOI5LSeClRZk2?usp=sharing) |
+| Burnett | 5957.4 | 731.2 | -87.73 | [checkpoint](https://drive.google.com/drive/folders/1mp4n1uPkRVhe27OpUUnkoE726Wx2agYL?usp=sharing) |
+| Chowilla | 158.7 | 16.8 | -89.40 | [checkpoint](https://drive.google.com/drive/folders/1m5hnVxXkf6R8kkpJbeKljij_TqobRqSf?usp=sharing) |
 
-
+After downloading the model checkpoints, edit the appropriate variables in the config file:
+```json
+"resume_state": "/path/to/pretrained/model",
+"vae_state": "/path/to/pretrained/vae",
+"vae_dem_state": "/path/to/pretrained/dem/vae"
+```
+Do not include the `_gen.pth`/`_opt.pth` in the path to the pretrained LDM, the parser will autorimatically detect both files as long as they are in the same directory.
 
 ## Requirements
 Python 3.10.12+
@@ -19,7 +29,9 @@ pip install -r requirements.txt
 ```
 
 ## Config
-Different configuration options for training and testing of models are listed in `config.json`. See `config` directory for examples of config files.
+Different configuration options for training and testing of models are specified in a json file and can be passed into the training/testing command via a command-line argument. The `config` directory contains config files for reference, including examples of a [standard diffusion model](config/wollombi-5-01norm.json) and a [latent diffusion model](config/wollombi-trnf.json). 
+
+2 diffusion model architectures are available in this codebase, adapted from the [SR3](https://arxiv.org/abs/2104.07636) and [DDPM](https://arxiv.org/abs/2006.11239) papers. The desired model architecture can be selected using the `which_model_G` variable in the config file. It is recommended to use the sr3 architecture for standard diffusion models and the ddpm architecture for latent diffusion models.
 
 ## Catchment data structure
 Catchment data should be stored in this folder structure:
@@ -49,15 +61,18 @@ Catchment data should be stored in this folder structure:
 ```
 
 ## Training
+Training is done with the following command:
 ```
 python sr.py -p train -c config.json -output-dir path/to/output/dir
 ```
+The model checkpoints will be stored in the output directory.
 
 ## Testing
+Testing is done with the following command:
 ```
 python sr.py -p test -c config.json -output-dir path/to/output/dir
 ```
+The super-resolution model outputs will be saved in the output directory if the corresponding option is set in the config file.
 
 ## Acknowledgements
-
 Code was adapted from [this implementation](https://github.com/Janspiry/Image-Super-Resolution-via-Iterative-Refinement/tree/master) of [Image Super-Resolution via Iterative Refinement](https://arxiv.org/pdf/2104.07636)
--- sr.py
+++ sr.py
@@ -66,52 +66,55 @@ def test(diffusion, opt, test_loader, progress_bar=True):
     logger = logging.getLogger('base')
     
     if progress_bar:
-        test_loader = tqdm(test_loader, desc=f"Test batch")
+        test_iterator = tqdm(test_loader, desc=f"Test batch")
+    else:
+        test_iterator = test_loader
     start_time = time.time()
-    imgs = []
-    for val_data in test_loader:
+    # imgs = []
+    for val_data in test_iterator:
         diffusion.feed_data(val_data)
         diffusion.test()
         visuals = diffusion.get_current_visuals()
         sr_image = visuals['SR'].squeeze().clamp(norm_range[0], norm_range[1])
         fg_image = visuals['HR'].squeeze()
         cg_image = visuals['INF'].squeeze()
-        imgs.append(sr_image.cpu().detach().numpy())
+        
+        # imgs.append(sr_image.cpu().detach().numpy())
 
         total_mse_input += mse_loss_sum(cg_image, fg_image).item()
         total_mse_predicted += mse_loss_sum(sr_image, fg_image).item()
 
-        fg_flood_map = Metrics.unnormalize(fg_image, max_depth, min_max=norm_range)
-        cg_flood_map = Metrics.unnormalize(cg_image, max_depth, min_max=norm_range)
-        sr_flood_map = Metrics.unnormalize(sr_image, max_depth, min_max=norm_range)
+        if opt["test"]["thresholds"]:
+            fg_flood_map = Metrics.unnormalize(fg_image, max_depth, min_max=norm_range)
+            cg_flood_map = Metrics.unnormalize(cg_image, max_depth, min_max=norm_range)
+            sr_flood_map = Metrics.unnormalize(sr_image, max_depth, min_max=norm_range)
 
-        classification_results = {
-            threshold: {
-                map_type: {
-                    "tp": 0,
-                    "fp": 0,
-                    "fn": 0,
-                    "tn": 0
-                } for map_type in ("CG", "SR")
-            } for threshold in opt["test"]["thresholds"]
-        }
+            classification_results = {
+                threshold: {
+                    map_type: {
+                        "tp": 0,
+                        "fp": 0,
+                        "fn": 0,
+                        "tn": 0
+                    } for map_type in ("CG", "SR")
+                } for threshold in opt["test"]["thresholds"]
+            }
 
-        for threshold in opt["test"]["thresholds"]:
-            fg_bin = torch.where(fg_flood_map < threshold, False, True)
-            cg_bin = torch.where(cg_flood_map < threshold, False, True)
-            sr_bin = torch.where(sr_flood_map < threshold, False, True)
+            for threshold in opt["test"]["thresholds"]:
+                fg_bin = torch.where(fg_flood_map < threshold, False, True)
+                cg_bin = torch.where(cg_flood_map < threshold, False, True)
+                sr_bin = torch.where(sr_flood_map < threshold, False, True)
 
-            classification_results[threshold]["CG"]["tp"] += torch.sum(torch.logical_and(cg_bin, fg_bin))
-            classification_results[threshold]["CG"]["fp"] += torch.sum(torch.logical_and(cg_bin, torch.logical_not(fg_bin)))
-            classification_results[threshold]["CG"]["fn"] += torch.sum(torch.logical_and(torch.logical_not(cg_bin), fg_bin))
-            classification_results[threshold]["CG"]["tn"] += torch.sum(torch.logical_and(torch.logical_not(cg_bin), torch.logical_not(fg_bin)))
+                classification_results[threshold]["CG"]["tp"] += torch.sum(torch.logical_and(cg_bin, fg_bin))
+                classification_results[threshold]["CG"]["fp"] += torch.sum(torch.logical_and(cg_bin, torch.logical_not(fg_bin)))
+                classification_results[threshold]["CG"]["fn"] += torch.sum(torch.logical_and(torch.logical_not(cg_bin), fg_bin))
+                classification_results[threshold]["CG"]["tn"] += torch.sum(torch.logical_and(torch.logical_not(cg_bin), torch.logical_not(fg_bin)))
 
-            classification_results[threshold]["SR"]["tp"] += torch.sum(torch.logical_and(sr_bin, fg_bin))
-            classification_results[threshold]["SR"]["fp"] += torch.sum(torch.logical_and(sr_bin, torch.logical_not(fg_bin)))
-            classification_results[threshold]["SR"]["fn"] += torch.sum(torch.logical_and(torch.logical_not(sr_bin), fg_bin))
-            classification_results[threshold]["SR"]["tn"] += torch.sum(torch.logical_and(torch.logical_not(sr_bin), torch.logical_not(fg_bin)))
+                classification_results[threshold]["SR"]["tp"] += torch.sum(torch.logical_and(sr_bin, fg_bin))
+                classification_results[threshold]["SR"]["fp"] += torch.sum(torch.logical_and(sr_bin, torch.logical_not(fg_bin)))
+                classification_results[threshold]["SR"]["fn"] += torch.sum(torch.logical_and(torch.logical_not(sr_bin), fg_bin))
+                classification_results[threshold]["SR"]["tn"] += torch.sum(torch.logical_and(torch.logical_not(sr_bin), torch.logical_not(fg_bin)))
         
-
         if opt["phase"] == "test" and opt["test"]["save_results"]:
             sr_flood_map = Metrics.tensor2floodmap(
                 sr_image, 
@@ -138,24 +141,25 @@ def test(diffusion, opt, test_loader, progress_bar=True):
     total_mse_predicted *= max_depth ** 2
     total_mse_predicted /= mse_coeff
 
-    for threshold in classification_results:
-        for map_type, results_dict in classification_results[threshold].items():
-            results_dict["pod"] = results_dict["tp"] / (results_dict["tp"] + results_dict["fn"])
-            results_dict["rfa"] = results_dict["fp"] / (results_dict["tp"] + results_dict["fp"])
-            results_dict["csi"] = results_dict["tp"] / (results_dict["tp"] + results_dict["fn"] + results_dict["fp"])
-
     # log
     logger.info(f"# Validation # MSE (CG to FG): {total_mse_input:.4f}")
     logger.info(f"# Validation # MSE (SR to FG): {total_mse_predicted:.4f}")
-    for threshold in classification_results:
-        for map_type in classification_results[threshold]:
-            logger.info(f"# Threshold: {threshold}cm # POD ({map_type} to FG): {classification_results[threshold][map_type]['pod']:.4f}")
-            logger.info(f"# Threshold: {threshold}cm # RFA ({map_type} to FG): {classification_results[threshold][map_type]['rfa']:.4f}")
-            logger.info(f"# Threshold: {threshold}cm # CSI ({map_type} to FG): {classification_results[threshold][map_type]['csi']:.4f}")
-
+    if opt["test"]["thresholds"]:
+        for threshold in classification_results:
+            for map_type, results_dict in classification_results[threshold].items():
+                results_dict["pod"] = results_dict["tp"] / (results_dict["tp"] + results_dict["fn"])
+                results_dict["rfa"] = results_dict["fp"] / (results_dict["tp"] + results_dict["fp"])
+                results_dict["csi"] = results_dict["tp"] / (results_dict["tp"] + results_dict["fn"] + results_dict["fp"])
+                logger.info(f"# Threshold: {threshold}cm # POD ({map_type} to FG): {results_dict['pod']:.4f}")
+                logger.info(f"# Threshold: {threshold}cm # RFA ({map_type} to FG): {results_dict['rfa']:.4f}")
+                logger.info(f"# Threshold: {threshold}cm # CSI ({map_type} to FG): {results_dict['csi']:.4f}")
     logger.info(f"# Time taken: {str(time_taken)}")
 
-    return total_mse_input, total_mse_predicted, classification_results, time_taken, np.concatenate(imgs)
+    if opt["test"]["thresholds"]:
+        return total_mse_input, total_mse_predicted, classification_results, time_taken
+    else:
+        return total_mse_input, total_mse_predicted, None, time_taken
+    # return total_mse_input, total_mse_predicted, time_taken
 
 
 if __name__ == "__main__":
@@ -212,34 +216,44 @@ if __name__ == "__main__":
             input_mse, predicted_mse = [], []
             threshold_csi = {
                 threshold: {
-                    map_type: 0 for map_type in ("CG", "SR")
+                    map_type: {
+                        "pod": 0,
+                        "rfa": 0,
+                        "csi": 0
+                    } for map_type in ("CG", "SR")
                 } for threshold in opt["test"]["thresholds"]
-            } 
+            }
             total_duration = datetime.timedelta()
             all_maps = []
             for i in range(1, num_epochs + 1):
                 logger.info(f"Test epoch {i}/{num_epochs}:")
-                epoch_input_mse, epoch_predicted_mse, epoch_classification_results, duration, epoch_maps = test(
-                    diffusion, opt, test_loader, progress_bar=False)
+                epoch_input_mse, epoch_predicted_mse, epoch_classification_results, duration = test(diffusion, opt, test_loader, progress_bar=False)
                 input_mse.append(epoch_input_mse)
                 predicted_mse.append(epoch_predicted_mse)
-                for threshold in threshold_csi:
-                    for map_type in threshold_csi[threshold]:
-                        threshold_csi[threshold][map_type] += epoch_classification_results[threshold][map_type]['csi']
                 total_duration += duration
-                all_maps.append(epoch_maps)
+                if opt["test"]["thresholds"]:
+                    for threshold in threshold_csi:
+                        for map_type in threshold_csi[threshold]:
+                            threshold_csi[threshold][map_type]['pod'] += epoch_classification_results[threshold][map_type]['pod']
+                            threshold_csi[threshold][map_type]['rfa'] += epoch_classification_results[threshold][map_type]['rfa']
+                            threshold_csi[threshold][map_type]['csi'] += epoch_classification_results[threshold][map_type]['csi']
+                # all_maps.append(epoch_maps)
             average_time = total_duration / num_epochs
             average_input_mse = sum(input_mse) / len(input_mse)
             average_predicted_mse = sum(predicted_mse) / len(predicted_mse)
             logger.info(f"# Average MSE (CG to FG): {average_input_mse:.4f}")
             logger.info(f"# Average MSE (SR to FG): {average_predicted_mse:.4f}")
-            for threshold in threshold_csi:
-                for map_type in threshold_csi[threshold]:
-                    threshold_csi[threshold][map_type] /= num_epochs
-                    logger.info(f"# Threshold {threshold}cm # Average CSI: {threshold_csi[threshold][map_type]:.4f}")
-            variance = np.var(all_maps, axis=0)
-            standard_dev = np.std(all_maps, axis=0)
-            logger.info(f"Variance ({variance.shape}): {np.mean(variance):.2E}, Standard Deviation ({standard_dev.shape}): {np.mean(standard_dev):.2E}")
-            # logger.info(f"Variance: {np.var(predicted_mse):.2f}, Standard Deviation: {np.std(predicted_mse):.2f}")
+            if opt["test"]["thresholds"]:
+                for threshold in threshold_csi:
+                    for map_type in threshold_csi[threshold]:
+                        threshold_csi[threshold][map_type]['pod'] /= num_epochs
+                        threshold_csi[threshold][map_type]['rfa'] /= num_epochs
+                        threshold_csi[threshold][map_type]['csi'] /= num_epochs
+                        logger.info(f"# Threshold {threshold}cm # Average POD ({map_type} to FG): {threshold_csi[threshold][map_type]['pod']:.4f}")
+                        logger.info(f"# Threshold {threshold}cm # Average RFA ({map_type} to FG): {threshold_csi[threshold][map_type]['rfa']:.4f}")
+                        logger.info(f"# Threshold {threshold}cm # Average CSI ({map_type} to FG): {threshold_csi[threshold][map_type]['csi']:.4f}")
+            # variance = np.var(all_maps, axis=0)
+            # standard_dev = np.std(all_maps, axis=0)
+            # logger.info(f"Variance ({variance.shape}): {np.mean(variance):.2E}, Standard Deviation ({standard_dev.shape}): {np.mean(standard_dev):.2E}")
             logger.info(f"Average time taken: {str(average_time)}")
         logger.info('End of testing.')
